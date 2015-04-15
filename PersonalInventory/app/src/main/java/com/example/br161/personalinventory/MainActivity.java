540767913bfe4fe2ced3bcc3ecc3b0f25c2ad109
rosskyl <rosskyl@mnstate.edu>
2015-04-15 16:35:33 -0500

    Added push notifications

--- PersonalInventory/app/src/main/java/com/example/br161/personalinventory/MainActivity.java
+++ PersonalInventory/app/src/main/java/com/example/br161/personalinventory/MainActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +20,18 @@ public class MainActivity extends Activity {
         //needed for using parse
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "niksKHirkYlRWqK36F6VQhSqUQ7XIfF25sILpM7N", "I4kaV9SnJWnACD2Uc4JkXDTKD1GFPDxSmpKFGkpp");
+
+        //needed for push notifications
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }//end else
+            }//end done
+        });//end ParsePush.subscribeInBackground
 
         Inventory inventory = new Inventory();
         boolean test = inventory.putItem("asdfas ", "", 1);
